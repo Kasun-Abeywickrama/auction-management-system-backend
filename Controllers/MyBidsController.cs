2f0f979890a1d7fa2c99771347d3729e9036f6aa
Dewmini-Mandis <136433945+Dewmini-Mandis@users.noreply.github.com>
2024-10-24 12:56:33 +0530

    upload mybidscontroller

---
+++ Controllers/MyBidsController.cs
@@ -0,0 +1,31 @@
+﻿using AuctionManagementAPI.Data;
+using AuctionManagementAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionManagementAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyBidsController : ControllerBase
+    {
+        private readonly AuctionContext auctionContext;
+
+        public MyBidsController(AuctionContext auctionContext)
+        {
+            this.auctionContext = auctionContext;
+        }
+
+        [HttpGet("DisplayMyBids")]
+        public IActionResult GetAllMyBids()
+        {
+            var bids = auctionContext.Bids.ToList();
+
+            if (bids.Count == 0)
+            {
+                return NotFound("No bids found");
+            }
+            return Ok(bids);
+        }
+    }
+}
